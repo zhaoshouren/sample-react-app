5ac993a53eb41185ef9d770a4585e8b399d9ff02
Warren Chu <warren.chu@irs.gov>
2019-06-12 21:44:17 -0400

    Update index.js

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,17 +1,20 @@
+//read from .env files or from window.env for projects importing this service but not using React
 const gaMeasurementId = process.env.REACT_APP_GA_MEASUREMENT_ID || (window.env && window.env.GA_MEASUREMENT_ID);
 
-export function send(event) {
+//TODO: refactor to return a promise to make this thenable.
+export function send(event) { //event follows format EVENT constant below
     if (gaMeasurementId) {
-        setTimeout(() => {       
-            //check if gtag initialized
+        //use setTimeout to make this a async call
+        setTimeout(() => {
+            //conditionally initialize gtag 
             if (window && !window.gtag) {
                 const body = document.getElementsByTagName("body")[0];
-                
+
                 const externalScript = document.createElement("SCRIPT");
                 externalScript.setAttribute("async", true);
                 externalScript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?" + gaMeasurementId);
                 body.appendChild(externalScript);
-        
+
                 const script = document.createElement("SCRIPT");
                 script.innerHTML = 'window.dataLayer = window.dataLayer || [];\n'
                     + '\tfunction gtag(){dataLayer.push(arguments);}\n'
@@ -19,7 +22,8 @@ export function send(event) {
                     + '\tgtag(\'config\', \'' + gaMeasurementId + '\');';
                 body.appendChild(script);
             }
-        
+
+            //check if event conforms to minimum needed by window.gtag
             if (event && event.name) {
                 window.gtag('event', event.name, event.parameters);
             } else {
@@ -39,11 +43,17 @@ export function send(event) {
     }
 }
 
+//Add commonly used events here for unique events you can pass object literals
 export const EVENT = {
-    "getTranscript" : {
-        "name": "get_transcript",
-        "parameters" : {
+    getTranscript: {
+        name: "get_transcript",
+        parameters: {
             //
         }
+    },
+    goToSettings: {
+        name: "go_to_settings",
+        parameters: {}
     }
+
 };
